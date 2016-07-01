6dbcb6f93d51493b9ac20807ee1e0194f1247c84
Boyd Bueno de Mesquita <boydbuenodemesquita@gmail.com>
2016-07-01 03:05:43 +0200

    Added bunch of stuff for the demo!

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -3,28 +3,37 @@ using System.Collections;
 
 public class GameManager : MonoBehaviour {
 
-    public Ship player;
-
-    private SerialPortManager serialPortManager;
-
-    void Start() {
-        serialPortManager = SerialPortManager.Instance;
-    }
+    public Ship Ship;
 
     void Update() {
         if (Input.GetButtonDown("start")) {
-            player.SetKeySwitchState(1);
-            player.SetEngineState(1);
+            Ship.SetKeySwitchState(1);
+            Ship.SetEngineState(1);
         }
 
         if (Input.GetAxisRaw("vertical") != 0 || Input.GetAxis("verticalAxis") != 0 || Input.GetAxis("verticalLeftAnalog") != 0) {
             float value = (Input.GetAxisRaw("vertical") + Input.GetAxis("verticalAxis") + Input.GetAxis("verticalLeftAnalog")) * Time.deltaTime * 2;
-            player.ModifyThrust(value);
+            Ship.ModifyStarboardThrust(value);
         }
 
-        if (Input.GetAxisRaw("horizontal") != 0 || Input.GetAxis("horizontalAxis") != 0 || Input.GetAxis("horizontalLeftAnalog") != 0) {
-            float value = (Input.GetAxisRaw("horizontal") + Input.GetAxis("horizontalAxis") + Input.GetAxis("horizontalLeftAnalog")) * Time.deltaTime * 50;
-            player.ModifyRotation(value);
+        if (Input.GetAxisRaw("modifyValue") != 0) {
+            Ship.ModifyPortThrust(Input.GetAxisRaw("modifyValue") * Time.deltaTime * 2);
+        }
+
+        if (Input.GetButtonDown("boost")) {
+            Ship.ToggleBoost();
+        }
+
+        if (Input.GetButtonDown("fire")) {
+            Ship.Fire();
+        }
+
+        if (Input.GetButtonDown("keyswitch")) {
+            Ship.SetSecondKeySwitchState(1);
+        }
+
+        if (Input.GetButtonUp("keyswitch")) {
+            Ship.SetSecondKeySwitchState(0);
         }
     }
 
@@ -32,25 +41,25 @@ public class GameManager : MonoBehaviour {
         string[] commandParts = commandString.Split(':');
         string command = commandParts[0];
         float value = float.Parse(commandParts[1]);
-
+        Debug.Log(command);
         switch(command) {
             case "SLIDER":
-                player.SetThrust(value);
+                Ship.SetPortThrust(value);
                 break;
             case "BOOST_SWITCH":
-                player.SetBoostState(value);
+                Ship.SetBoostState(value);
                 break;
             case "ROTARY":
-                player.Rotate(value);
+                Ship.RotateGun(value);
                 break;
             case "TEST_SWITCH":
-                player.SetShieldsState(value);
+                Ship.SetShieldsState(value);
                 break;
             case "KEY_SWITCH":
-                player.SetKeySwitchState(value);
+                Ship.SetKeySwitchState(value);
                 break;
             case "START_BTN":
-                player.SetEngineState(value);
+                Ship.SetEngineState(value);
                 break;
         }
     }
--- Assets/Scripts/Ship.cs
+++ Assets/Scripts/Ship.cs
@@ -5,25 +5,39 @@ using System;
 public class Ship : MonoBehaviour {
 
     public GameObject Shield;
+    public GameObject ThrusterPortside;
+    public ParticleSystem PortFlare;
+    public ParticleSystem PortCore;
+    public GameObject ThrusterStarboardside;
+    public ParticleSystem StarboardFlare;
+    public ParticleSystem StarboardCore;
+    public GameObject BoostThruster;
+
+    public GameObject Gun;
+    public GameObject LaserBullet;
+    public float targetGunRotation;
+    private float maxGunRotationSpeed = 20;
+
+    private float flareMinSize = 0.5f;
+    private float flareGrowth = 0.3f;
+    private float coreMinSize = 0.4f;
+    private float coreGrowth = 0.9f;
 
     private bool isKeySwitchActive = false;
-    public bool isReactorActive = false;
+    private bool isKeySwitch2Active = false;
+    private bool isReactorActive = false;
 
-    private float maxThrust = 4;
+    private float maxThrust = 2;
 
-    private float boostThrust = 15;
+    private float boostThrust = 20;
 
     private bool isBoostActive = false;
 
-    private float maxRotationSpeed = 40;
-
-    private float targetRotation;
-
-    public float thrust = 0;
+    public float thrustPort = 0;
+    public float thrustStarboard = 0;
 
     private SerialPortManager serialPortManager;
     private Rigidbody2D rigidbody;
-
 
     void Start () {
         serialPortManager = SerialPortManager.Instance;
@@ -33,43 +47,116 @@ public class Ship : MonoBehaviour {
     }
 
 	void FixedUpdate () {
-        if (transform.localEulerAngles.z != targetRotation && isReactorActive) {
-            HandleRotation();
-        }
-
         if (isReactorActive) {
+
+            if (Gun.transform.localEulerAngles.z != targetGunRotation) {
+                HandleGunRotation();
+            }
+
             if (isBoostActive) {
-                rigidbody.AddRelativeForce(new Vector2(0, boostThrust));
+                rigidbody.AddForceAtPosition(boostThrust * transform.up, BoostThruster.transform.position, ForceMode2D.Force);
             } else {
-                rigidbody.AddRelativeForce(new Vector2(0, thrust));
+                rigidbody.AddForceAtPosition(thrustPort * transform.up, ThrusterPortside.transform.position, ForceMode2D.Force);
+                rigidbody.AddForceAtPosition(thrustStarboard * transform.up, ThrusterStarboardside.transform.position, ForceMode2D.Force);
             }
         }
     }
 
-    public void SetThrust(float value) {
-        thrust = value * maxThrust;
+    public void Fire() {
+        GameObject laser = (GameObject)Instantiate(LaserBullet, Gun.transform.position, Gun.transform.rotation);
+        Rigidbody rigidbody = laser.GetComponent<Rigidbody>();
+        rigidbody.AddForce(new Vector3(0, 15, 0));
     }
 
-    public void ModifyThrust(float value) {
-        if ((value > 0 && thrust == maxThrust) || (value < 0 && thrust == 0)) return;
-
-        thrust += value;
-
-        if (thrust > maxThrust) {
-            thrust = maxThrust;
-        } else if (thrust < 0) {
-            thrust = 0;
+    public void RotateGun(float rotate) {
+        if (rotate < 0.5f) { // right
+            float diffPerc = rotate / 0.5f;
+            targetGunRotation = 40 - (40 * diffPerc);
+        } else { // left
+            float diffPerc = (rotate - 0.5f) / 0.5f;
+            targetGunRotation = 360 - (40 * diffPerc);
         }
     }
 
-    public void ModifyRotation(float value) {
-        targetRotation += value;
+    private void HandleGunRotation() { 
+        float diff = Gun.transform.localEulerAngles.y - targetGunRotation;
+
+        if ((diff < 0 && diff > -180) || (diff > 0 && diff > 180)) {
+            if (Math.Abs(diff) < maxGunRotationSpeed * Time.deltaTime) {
+                Gun.transform.localEulerAngles = new Vector3(0, targetGunRotation, 0);
+            } else {
+                Gun.transform.localEulerAngles = new Vector3(0, Gun.transform.localEulerAngles.y + (maxGunRotationSpeed * Time.deltaTime), 0);
+            }
+        } else {
+            if (Math.Abs(diff) < maxGunRotationSpeed * Time.deltaTime) {
+                Gun.transform.localEulerAngles = new Vector3(0, targetGunRotation, 0);
+            } else {
+                Gun.transform.localEulerAngles = new Vector3(0, Gun.transform.localEulerAngles.y - (maxGunRotationSpeed * Time.deltaTime), 0);
+            }
+        }
     }
 
-    public void Rotate(float rotate) {
-        targetRotation = (360 * rotate) - 180;
-        if (targetRotation < 0)
-            targetRotation = 360 + targetRotation;
+
+    public void SetPortThrust(float value) {
+        UpdatePortThrust(value * maxThrust);
+    }
+
+    public void ModifyPortThrust(float value) {
+        if ((value > 0 && thrustPort == maxThrust) || (value < 0 && thrustPort == 0)) return;
+
+        UpdatePortThrust(thrustPort + value);
+
+        if (thrustPort > maxThrust) {
+            UpdatePortThrust(maxThrust);
+        } else if (thrustPort < 0) {
+            UpdatePortThrust(0);
+        }
+    }
+
+    private void UpdatePortThrust(float value) {
+        thrustPort = value;
+
+        float thrustPercentage = value / maxThrust;
+
+        PortFlare.startSize = flareMinSize + (flareGrowth * thrustPercentage);
+        
+        if (value > maxThrust * 0.25f) {
+            PortCore.gameObject.SetActive(true);
+            PortCore.startSize = coreMinSize + (coreGrowth * thrustPercentage);
+        } else {
+            PortCore.gameObject.SetActive(false);
+        }
+    }
+
+    public void SetStarboardThrust(float value) {
+        UpdateStartboardThrust(value * maxThrust);
+    }
+
+    public void ModifyStarboardThrust(float value) {
+        if ((value > 0 && thrustStarboard == maxThrust) || (value < 0 && thrustStarboard == 0)) return;
+
+        UpdateStartboardThrust(thrustStarboard + value);
+
+        if (thrustStarboard > maxThrust) {
+            UpdateStartboardThrust(maxThrust);
+        } else if (thrustStarboard < 0) {
+            UpdateStartboardThrust(0);
+        }
+    }
+
+    private void UpdateStartboardThrust(float value) {
+        thrustStarboard = value;
+
+        float thrustPercentage = value / maxThrust;
+
+        StarboardFlare.startSize = flareMinSize + (flareGrowth * thrustPercentage);
+
+        if (value > maxThrust * 0.25f) {
+            StarboardCore.gameObject.SetActive(true);
+            StarboardCore.startSize = coreMinSize + (coreGrowth * thrustPercentage);
+        } else {
+            StarboardCore.gameObject.SetActive(false);
+        }
     }
 
     public void SetShieldsState(float value) {
@@ -85,7 +172,8 @@ public class Ship : MonoBehaviour {
     public void SetKeySwitchState(float value) {
         if (value == 1) {
             isKeySwitchActive = true;
-            serialPortManager.AddToSerialQueue("ENG:1");
+            if (isKeySwitch2Active)
+                serialPortManager.AddToSerialQueue("ENG:1");
         } else {
             isKeySwitchActive = false;
         }
@@ -95,14 +183,48 @@ public class Ship : MonoBehaviour {
         }
     }
 
+    public void SetSecondKeySwitchState(float value) {
+        if (value == 1) {
+            isKeySwitch2Active = true;
+            if (isKeySwitchActive)
+                serialPortManager.AddToSerialQueue("ENG:1");
+        } else {
+            isKeySwitch2Active = false;
+        }
+
+
+        if (value == 0 && !isReactorActive) {
+            serialPortManager.AddToSerialQueue("ENG:0");
+        }
+    }
+
     public void SetEngineState(float value) {
         if (!isReactorActive && isKeySwitchActive && value == 1) {
             isReactorActive = true;
+            ActivateThrusters();
+            ActivateGun();
         }
+    }
+
+    public void ActivateThrusters() {
+        PortFlare.gameObject.SetActive(true);
+        StarboardFlare.gameObject.SetActive(true);
+    }
+
+    public void ActivateGun() {
+        Gun.gameObject.SetActive(true);
     }
 
     public void SetBoostState(float value) {
         if (value == 0) {
+            DeactivateBoost();
+        } else {
+            ActivateBoost();
+        }
+    }
+
+    public void ToggleBoost() {
+        if (isBoostActive) {
             DeactivateBoost();
         } else {
             ActivateBoost();
@@ -115,24 +237,6 @@ public class Ship : MonoBehaviour {
 
     private void DeactivateBoost() {
         isBoostActive = false;
-    }
-
-    private void HandleRotation() {
-        float diff = transform.localEulerAngles.z - targetRotation;
-
-        if ((diff < 0 && diff > -180) || (diff > 0 && diff > 180)) {
-            if (Math.Abs(diff) < maxRotationSpeed * Time.deltaTime) {
-                transform.localEulerAngles = new Vector3(0, 0, targetRotation);
-            } else {
-                transform.localEulerAngles = new Vector3(0, 0, transform.localEulerAngles.z + (maxRotationSpeed * Time.deltaTime));
-            }
-        } else {
-            if (Math.Abs(diff) < maxRotationSpeed * Time.deltaTime) {
-                transform.localEulerAngles = new Vector3(0, 0, targetRotation);
-            } else {
-                transform.localEulerAngles = new Vector3(0, 0, transform.localEulerAngles.z - (maxRotationSpeed * Time.deltaTime));
-            }
-        }
     }
 
     private void DeactivateAllSystems() {
